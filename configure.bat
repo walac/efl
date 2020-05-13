473f451578475ee072cac853075d52f59a857707
Joao Antonio Cardoso <joao.antonio@expertisesolutions.com.br>
2020-05-13 18:16:05 -0300

    Adding nuget-download option for meson

--- configure.bat
+++ configure.bat
@@ -89,6 +89,7 @@ exit /B 0
             -Dbuild-tests=false^
             -Dbuild-examples=false^
             -Dbindings=^
+            -Dnuget-download=true^
             --native-file native-file-windows.txt
 
     if exist build (
